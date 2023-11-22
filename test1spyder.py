aa887e6b8ecbdb50fe6828c3c505f7cd13f19f17
Eduardo Fletes Arechiga <eduardo.fa@outlook.com>
2023-11-21 19:01:58 -0600

    primer commit hecho

---
+++ test1spyder.py
@@ -0,0 +1,14 @@
+def multiplica(a,b): 
+    variable1=a*2
+    variable2=b*3
+    resultado=variable1*variable2
+    print("empezando for")
+
+    for i in range(1, 5): 
+        print(i)
+        resultado=resultado * i
+        
+        print(resultado)
+    
+multiplica(3,6)
+        
